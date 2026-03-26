9b7ef705fb643d8c08f74845f3d6d5c38b3c92ef
kk <kklam6077@gmail.com>
2026-03-26 09:51:12 +0000

    Day 1 Claude Homework

---
+++ diskcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+read -p "Enter Threshold" THRESHOLD
+THRESHOLD=${THRESHOLD:-80}
+echo "$THRESHOLD"
+
+LOGFILE="/tmp/disk-alert.log"
+
+df -h | awk 'NR>1{gsub("%","",$5);if ($5+0>THRESH) print$0}' \
+THRESH=$THRESHOLD | while read line; do echo "[$(date)] Warning: $line" | tee -a $LOGFILE
+done
